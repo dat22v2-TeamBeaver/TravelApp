ffc69dd14381b3295849e0926f42c688d4dd8d54
Mark <mark771e@stud.kea.dk>
2023-04-18 11:39:50 +0200

    git init

---
+++ src/test/java/dat22v2/tb/travelapp/TravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dat22v2.tb.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
