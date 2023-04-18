24bffc637c57fc5686716de19b5e29f7f1ad172d
Mark <mark771e@stud.kea.dk>
2023-04-18 13:25:53 +0200

    project setup

--- src/main/java/dat22v2/tb/travelapp/api/TravelController.java
+++ src/main/java/dat22v2/tb/travelapp/api/TravelController.java
@@ -1,4 +1,22 @@
 package dat22v2.tb.travelapp.api;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/travel")
 public class TravelController {
+
+
+
+    @GetMapping("/hey")
+    public ResponseEntity<Boolean> test() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
