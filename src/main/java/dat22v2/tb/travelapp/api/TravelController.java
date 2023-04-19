3442de9ebc31b50fbb2dc47bc9be4898e0e17250
bjoexxxx <bjoe6702@stud.kea.dk>
2023-04-19 10:11:12 +0200

    OpenWeatherMap api integration now working

--- src/main/java/dat22v2/tb/travelapp/api/TravelController.java
+++ src/main/java/dat22v2/tb/travelapp/api/TravelController.java
@@ -3,10 +3,13 @@ package dat22v2.tb.travelapp.api;
 
 import dat22v2.tb.travelapp.dto.TravelRequest;
 import dat22v2.tb.travelapp.dto.TravelResponse;
+import dat22v2.tb.travelapp.dto.WeatherResponse;
+import dat22v2.tb.travelapp.service.RemoteApiService;
 import dat22v2.tb.travelapp.service.TravelService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @CrossOrigin
@@ -14,10 +17,14 @@ import org.springframework.web.bind.annotation.*;
 public class TravelController {
 
 
+
+    RemoteApiService remoteApiService;
     TravelService travelService;
 
-    public TravelController(TravelService travelService) {
+    public TravelController(TravelService travelService,
+                            RemoteApiService remoteApiService) {
         this.travelService = travelService;
+        this.remoteApiService = remoteApiService;
     }
 
     @GetMapping("/hey")
@@ -36,5 +43,10 @@ public class TravelController {
 
 
         return new TravelResponse();
+    }
+
+    @GetMapping("/weather/{location}")
+    public Mono<WeatherResponse> getWeather(@PathVariable String location){
+        return remoteApiService.atmosphereResponse(location);
     }
 }
---
+++ src/main/java/dat22v2/tb/travelapp/service/RemoteApiService.java
@@ -0,0 +1,27 @@
+package dat22v2.tb.travelapp.service;
+
+import dat22v2.tb.travelapp.dto.WeatherResponse;
+import dat22v2.tb.travelapp.utility.WeatherAPIKeyholder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RemoteApiService {
+
+    @Autowired
+    WeatherAPIKeyholder weatherAPIKeyholder;
+
+
+    public Mono<WeatherResponse> atmosphereResponse(String location){
+        WebClient webClient = WebClient.create();
+        Mono<WeatherResponse> response = webClient.get()
+                .uri("http://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="
+                        +weatherAPIKeyholder.getWeatherAPIKey())
+                .retrieve()
+                .bodyToMono(WeatherResponse.class);
+        return response;
+
+    }
+}
