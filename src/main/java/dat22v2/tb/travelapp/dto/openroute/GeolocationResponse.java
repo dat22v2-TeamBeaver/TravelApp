d28ae1c1ff76c277b1258a9e609d6decc53cde2e
Mark <mark771e@stud.kea.dk>
2023-04-19 12:26:38 +0200

    Added getters & setters for openRoute dtos and added functionality to add headers to callPostApi

--- src/main/java/dat22v2/tb/travelapp/dto/openroute/GeolocationResponse.java
+++ src/main/java/dat22v2/tb/travelapp/dto/openroute/GeolocationResponse.java
@@ -2,9 +2,15 @@ package dat22v2.tb.travelapp.dto.openroute;
 
 import dat22v2.tb.travelapp.dto.openroute.geolocation.Feature;
 import dat22v2.tb.travelapp.dto.openroute.geolocation.Geocoding;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class GeolocationResponse {
     public Geocoding geocoding;
     public String type;
--- src/main/java/dat22v2/tb/travelapp/dto/openroute/geolocation/Engine.java
+++ src/main/java/dat22v2/tb/travelapp/dto/openroute/geolocation/Engine.java
@@ -1,9 +1,16 @@
 package dat22v2.tb.travelapp.dto.openroute.geolocation;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 // import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
 // import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
 /* ObjectMapper om = new ObjectMapper();
 Root root = om.readValue(myJsonString, Root.class); */
+@Getter
+@Setter
+@NoArgsConstructor
 public class Engine {
     public String name;
     public String author;
